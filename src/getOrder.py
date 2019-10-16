b4a982903620bc58f59959fc5f3db5e8359edbc3
Matt Miller <mattmiller0526@gmail.com>
2019-10-16 15:39:58 -0500

    Added getOrderById
    added another get function and added a main()

--- src/getOrder.py
+++ src/getOrder.py
@@ -2,26 +2,36 @@ import requests
 import pprint
 import json
 
-def getAccountInfo(email, endpoint) :
-    account_response = requests.get(endpoint+'/Customer/byEmailWithAddressAndOrders?email='+email).json()
-    full_name = account_response["data"]["customerByEmail"]["firstName"]+' '+account_response["data"]["customerByEmail"]["lastName"]
+
+# define functions
+def getAccountInfo(email, endpoint):
+    account_response = requests.get(endpoint + '/Customer/byEmailWithAddressAndOrders?email=' + email).json()
+    full_name = account_response["data"]["customerByEmail"]["firstName"] + ' ' + \
+                account_response["data"]["customerByEmail"]["lastName"]
     return full_name
 
-endpoint = ('https://run.appstax.io/rest/369b58a7-bc4e-4fde-b1cb-c0b5023d48ce/master')
-pp = pprint.PrettyPrinter(indent=4)
 
-while True:
-    order_id = input("Order ID:")
-    # order_id = '6bb828c3-9060-4994-a235-ae6a54537772'
-    order_response = requests.get(endpoint+'/Order/byID?id='+order_id)
-    # order_response = requests.get(base_url +'/order/byOrderID?id=6bb828c3-9060-4994-a235-ae6a54537772')
-    data = order_response.json()
-    pp.pprint(data)
-    cust_email = data["data"]["order"]["customer"]["email"]
-    cust_name = getAccountInfo(cust_email, endpoint)
-    print(order_response)
-    print('Customer Name: '+ cust_name)
-    print('Customer Email: ' + cust_email)
-    print('\n------------------')
+def getOrderById(order_id, endpoint):
+    order = requests.get(endpoint + '/Order/byID?id=' + order_id).json()
+    return order
 
 
+#  main
+def main():
+    endpoint = ('https://run.appstax.io/rest/369b58a7-bc4e-4fde-b1cb-c0b5023d48ce/master')
+    pp = pprint.PrettyPrinter(indent=4)
+
+    while True:
+        order_id = input("Order ID:")
+        data = getOrderById(order_id, endpoint)
+        cust_name = getAccountInfo(cust_email, endpoint)
+        cust_email = data["data"]["order"]["customer"]["email"]
+
+        pp.pprint(data)
+        print('Customer Name: ' + cust_name)
+        print('Customer Email: ' + cust_email)
+        print('\n------------------')
+
+
+if __name__ == '__main__':
+    main()
